df5952a45804100ea55475c1df322fe2bd88d0d1
KamiKaguya <314191127@qq.com>
2024-12-16 03:18:57 +0800

    Port to 1.19.2

--- src/main/java/com/kamikaguya/ash_of_sin/world/effect/AshOfSinEffects.java
+++
@@ -1,9 +0,0 @@
-package com.kamikaguya.ash_of_sin.world.effect;
-
-import com.kamikaguya.ash_of_sin.main.AshOfSin;
-import net.minecraft.world.effect.MobEffect;
-import net.minecraftforge.registries.RegistryObject;
-
-public class AshOfSinEffects {
-    public static final RegistryObject<MobEffect> WRATH_OF_GOD = AshOfSin.WRATH_OF_GOD;
-}
